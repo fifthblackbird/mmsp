7c8831e504e5e768da5a4722e1282460071d8d3d
Trevor Keller <trevor.keller@nist.gov>
2016-03-07 20:06:23 -0500

    manufactured solution

--- examples/differential_equations/parabolic/heat/heat.cpp
+++ examples/differential_equations/parabolic/heat/heat.cpp
@@ -94,6 +94,7 @@ template <int dim, typename T> void update(grid<dim,T>& oldGrid, int steps)
     #ifdef MPI_VERSION
     rank = MPI::COMM_WORLD.Get_rank();
     #endif
+    static double elapsed = 0.0;
 
 	coefficients<dim> vars;
 
@@ -101,22 +102,60 @@ template <int dim, typename T> void update(grid<dim,T>& oldGrid, int steps)
 
 	grid<dim,T> newGrid(oldGrid);
 
+	// March forward in time
 	for (int step=0; step<steps; step++) {
-		if (rank==0)
-			print_progress(step, steps);
-
+		//if (rank==0)
+		//	print_progress(step, steps);
 		grid<dim,vector<T> > gradGrid(oldGrid,dim);
 		for (int n=0; n<nodes(oldGrid); n++) {
-			
+			vector<int> x = position(oldGrid,n);
+			double kappa = MS_k(vars.Ck,oldGrid(n));
+			gradGrid(x) = kappa*gradient(oldGrid,x);
 		}
 
 		for (int n=0; n<nodes(oldGrid); n++) {
-			newGrid(n) = 
-			phi-dt*M*(-r*phi+u*pow(phi,3)-K*laplacian(oldGrid,i));
+			vector<int> x = position(oldGrid,n);
+			T temp = oldGrid(n);
+			double prefactor = vars.dt/(vars.rho * MS_Cp(vars.Cc, temp));
+			double divKgrad = divergence(gradGrid,x);
+			double source = MS_Q(x, elapsed, vars.rho, vars.h, vars.Ax, vars.At, vars.Ck, vars.Cc);
+			newGrid(x) = temp + prefactor*(divKgrad + Q);
 		}
 		swap(oldGrid,newGrid);
 		ghostswap(oldGrid);
 	}
+
+	elapsed += vars.dt;
+
+	// Compute global error w.r.t. manufactured solution
+	double error = 0.0;
+	int npts = nodes(oldGrid);
+	for (int n=0; n<npts; n++) {
+		vector<int> x = position(oldGrid,n);
+		double analytical = MS_T(x, elapsed, vars.h, vars.Ax, vars.At);
+		double numerical = oldGrid(n);
+		error += pow(numerical - analytical, 2.0);
+	}
+	#ifdef MPI_VERSION
+	double myErr(error);
+	int myPts(npts);
+	MPI::COMM_WORLD.Allreduce(&myErr,&error,1,MPI_DOUBLE,MPI_SUM);
+	MPI::COMM_WORLD.Allreduce(&myPts,&npts,1,MPI_INT,MPI_SUM);
+	#endif
+	error = sqrt(error/npts);
+	std::cout<<elapsed<<'\t'<<error<<std::endl;
+
+}
+
+
+double MS_k (const vector<double>& Ck, double temp)
+{
+	return Ck[0] + Ck[1]*temp + Ck[2]*temp*temp;
+}
+
+double MS_Cp (const vector<double>& Cc, double temp)
+{
+	return Cc[0] + Cc[1]*temp + Cc[2]*temp*temp;
 }
 
 double MS_T (const vector<int>& xidx, double t, const vector<double>& h, const vector<double>& Ax, const vector<double>& At)
@@ -130,16 +169,6 @@ double MS_T (const vector<int>& xidx, double t, const vector<double>& h, const v
 	return (
 		cos(Ax[0] * x[0] + At[0] * t) * cos(Ax[1] * x[1] + At[1] * t) * cos(Ax[2] * x[2] + At[2] * t) * cos(At[3] * t)
 	);
-}
-
-double MS_k (const vector<double>& Ck, double temp)
-{
-	return Ck[0] + Ck[1]*temp + Ck[2]*temp*temp;
-}
-
-double MS_Cp (const vector<double>& Cc, double temp)
-{
-	return Cc[0] + Cc[1]*temp + Cc[2]*temp*temp;
 }
 
 vector<double> MS_GradT (const vector<int>& xidx, double t, const vector<double>& h, const vector<double>& Ax, const vector<double>& At)
@@ -162,71 +191,71 @@ double MS_Q (const vector<int>& xidx, double t, double rho, const vector<double>
 	vector<double> x(3,0.0); // most general case is 3D, zero by default to handle 1D and 2D naturally
 	for (int d=0; d<xidx.size(); d++)
 		x[d] = h[d]*xidx[d];
-	assert(Ax.size() == 3);
-	assert(k.size() == 3);
-	assert(Cp.size() == 3);
 	assert(At.size() == 4);
+	assert(Ax.size() == 3);
+	assert(Ck.size() == 3);
+	assert(Cc.size() == 3);
 
 	// Define the thermal source term. Transformed for MMSP accessors and restructured for clarity and linewidth
 	// after https://github.com/manufactured-solutions/analytical/tree/master/heat_equation/C_code/
 	// For best results, generate this yourself using a symbolic math package, e.g. SymPy, Mathematica, or Maple.
 	// All this floating-point math in a single expression makes me nervous.
 	double Q_T = 0.0;
-	Q_T = - rho * Cp[0] * At[0] * sin(Ax[0] * x[0] + At[0] * t)
+	Q_T = - rho * Cc[0] * At[0] * sin(Ax[0] * x[0] + At[0] * t)
 	                            * cos(Ax[1] * x[1] + At[1] * t)
 	                            * cos(Ax[2] * x[2] + At[2] * t)
 	                            * cos(At[3] * t)
-	      - rho * Cp[0] * At[1] * cos(Ax[0] * x[0] + At[0] * t)
+	      - rho * Cc[0] * At[1] * cos(Ax[0] * x[0] + At[0] * t)
 	                            * sin(Ax[1] * x[1] + At[1] * t)
 	                            * cos(Ax[2] * x[2] + At[2] * t)
 	                            * cos(At[3] * t)
-	      - rho * Cp[0] * At[2] * cos(Ax[0] * x[0] + At[0] * t)
+	      - rho * Cc[0] * At[2] * cos(Ax[0] * x[0] + At[0] * t)
 	                            * cos(Ax[1] * x[1] + At[1] * t)
 	                            * sin(Ax[2] * x[2] + At[2] * t)
 	                            * cos(At[3] * t)
-	      - rho * Cp[0] * At[3] * cos(Ax[0] * x[0] + At[0] * t)
+	      - rho * Cc[0] * At[3] * cos(Ax[0] * x[0] + At[0] * t)
 	                            * cos(Ax[1] * x[1] + At[1] * t)
 	                            * cos(Ax[2] * x[2] + At[2] * t)
 	                            * sin(At[3] * t)
-	      - k[1] * Ax[0]*Ax[0] * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
+	      - Ck[1] * Ax[0]*Ax[0] * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
 	                           * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
 	                           * pow(cos(At[3] * t), 2.0)
-	      - k[1] * Ax[1]*Ax[1] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
+	      - Ck[1] * Ax[1]*Ax[1] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
 	                           * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
 	                           * pow(cos(At[3] * t), 2.0)
-	      - k[1] * Ax[2]*Ax[2] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
+	      - Ck[1] * Ax[2]*Ax[2] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
 	                           * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
 	                           * pow(cos(At[3] * t), 2.0)
-	      + 3.0 * k[2] * ( Ax[0]*Ax[0] + Ax[1]*Ax[1] + Ax[2]*Ax[2] )
+	      + 3.0 * Ck[2] * ( Ax[0]*Ax[0] + Ax[1]*Ax[1] + Ax[2]*Ax[2] )
 	                   * pow(cos(Ax[0] * x[0] + At[0] * t), 3.0)
 	                   * pow(cos(Ax[1] * x[1] + At[1] * t), 3.0)
 	                   * pow(cos(Ax[2] * x[2] + At[2] * t), 3.0)
 	                   * pow(cos(At[3] * t), 3.0)
 	      + (
-	        - rho * Cp[1] * At[0] * sin(Ax[0] * x[0] + At[0] * t)
+	        - rho * Cc[1] * At[0] * sin(Ax[0] * x[0] + At[0] * t)
 	                              * cos(Ax[1] * x[1] + At[1] * t)
 	                              * cos(Ax[2] * x[2] + At[2] * t)
 	                              * cos(At[3] * t)
-	        - rho * Cp[1] * At[1] * cos(Ax[0] * x[0] + At[0] * t)
+	        - rho * Cc[1] * At[1] * cos(Ax[0] * x[0] + At[0] * t)
 	                              * sin(Ax[1] * x[1] + At[1] * t)
 	                              * cos(Ax[2] * x[2] + At[2] * t)
 	                              * cos(At[3] * t)
-	        - rho * Cp[1] * At[2] * cos(Ax[0] * x[0] + At[0] * t)
+	        - rho * Cc[1] * At[2] * cos(Ax[0] * x[0] + At[0] * t)
 	                              * cos(Ax[1] * x[1] + At[1] * t)
 	                              * sin(Ax[2] * x[2] + At[2] * t)
 	                              * cos(At[3] * t)
-	        - rho * Cp[1] * At[3] * cos(Ax[0] * x[0] + At[0] * t)
+	        - rho * Cc[1] * At[3] * cos(Ax[0] * x[0] + At[0] * t)
 	                              * cos(Ax[1] * x[1] + At[1] * t)
 	                              * cos(Ax[2] * x[2] + At[2] * t)
 	                              * sin(At[3] * t)
-	        + k[0] * Ax[0]*Ax[0] + k[0] * Ax[1]*Ax[1] + k[0] * Ax[2]*Ax[2]
-	        - 2.0 * k[2] * Ax[0]*Ax[0] * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
+	        + Ck[0] * Ax[0]*Ax[0] + Ck[0] * Ax[1]*Ax[1] + Ck[0] * Ax[2]*Ax[2]
+	        - 2.0 * Ck[2] * Ax[0]*Ax[0] * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
 	                                     * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
 	                                     * pow(cos(At[3] * t), 2.0)
-	        - 2.0 * k[2] * Ax[1]*Ax[1] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
+	        - 2.0 * Ck[2] * Ax[1]*Ax[1] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
 	                                     * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
 	                                     * pow(cos(At[3] * t), 2.0)
-	        - 2.0 * k[2] * Ax[2]*Ax[2] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
+	        - 2.0 * Ck[2] * Ax[2]*Ax[2] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
 	                                     * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
 	                                     * pow(cos(At[3] * t), 2.0)
 	        ) * cos(Ax[0] * x[0] + At[0] * t)
@@ -234,25 +263,25 @@ double MS_Q (const vector<int>& xidx, double t, double rho, const vector<double>
 	          * cos(Ax[2] * x[2] + At[2] * t)
 	          * cos(At[3] * t)
 	      + (
-	        - rho * Cp[2] * At[0] * sin(Ax[0] * x[0] + At[0] * t)
+	        - rho * Cc[2] * At[0] * sin(Ax[0] * x[0] + At[0] * t)
 	                              * cos(Ax[1] * x[1] + At[1] * t)
 	                              * cos(Ax[2] * x[2] + At[2] * t)
 	                              * cos(At[3] * t)
-	        - rho * Cp[2] * At[1] * cos(Ax[0] * x[0] + At[0] * t)
+	        - rho * Cc[2] * At[1] * cos(Ax[0] * x[0] + At[0] * t)
 	                              * sin(Ax[1] * x[1] + At[1] * t)
 	                              * cos(Ax[2] * x[2] + At[2] * t)
 	                              * cos(At[3] * t)
-	        - rho * Cp[2] * At[2] * cos(Ax[0] * x[0] + At[0] * t)
+	        - rho * Cc[2] * At[2] * cos(Ax[0] * x[0] + At[0] * t)
 	                              * cos(Ax[1] * x[1] + At[1] * t)
 	                              * sin(Ax[2] * x[2] + At[2] * t)
 	                              * cos(At[3] * t)
-	        - rho * Cp[2] * At[3] * cos(Ax[0] * x[0] + At[0] * t)
+	        - rho * Cc[2] * At[3] * cos(Ax[0] * x[0] + At[0] * t)
 	                              * cos(Ax[1] * x[1] + At[1] * t)
 	                              * cos(Ax[2] * x[2] + At[2] * t)
 	                              * sin(At[3] * t)
-	        + 2.0 * k[1] * Ax[0]*Ax[0]
-	        + 2.0 * k[1] * Ax[1]*Ax[1]
-	        + 2.0 * k[1] * Ax[2]*Ax[2]
+	        + 2.0 * Ck[1] * Ax[0]*Ax[0]
+	        + 2.0 * Ck[1] * Ax[1]*Ax[1]
+	        + 2.0 * Ck[1] * Ax[2]*Ax[2]
 	        ) * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
 	          * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
 	          * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
