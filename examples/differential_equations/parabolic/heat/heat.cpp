7963fbf64d89d18e0d57377426d566b9ad5cb3e5
Trevor Keller <trevor.keller@nist.gov>
2016-03-07 19:40:47 -0500

    manufactured coefficients, initialization

--- examples/differential_equations/parabolic/heat/heat.cpp
+++ examples/differential_equations/parabolic/heat/heat.cpp
@@ -12,27 +12,152 @@ using std::cos;
 
 namespace MMSP{
 
-
-double MS_T(const vector<double>& xcoeff, // spatial coefficients
-            const vector<double>& tcoeff, // temporal coefficients
-            double D,                     // time coefficient
-            const vector<double>& h,      // grid spacing
-            const vector<int>& x,         // spatial indices
-            double t)                     // time
+coefficients::coefficients() : // constructor
+		// call initializing constructor on vector-valued members to enforce 3 spatial dimensions (otherwise, high risk of segfaulting)
+		N(3,0),    // grid points
+		h(3,0.0),  // spatial resolution
+		Ax(3,0.0), // amplitudes in space
+		At(4,0.0), // amplitudes in time
+		Ck(3,0.0), // thermal conductivity polynomial coefficients
+		Cc(3,0.0)  // heat capacity polynomial coefficients
 {
-	// Return the manufactured solution T(x,y,z,t)
-	double temp = cos(D*t);
-	for (int i=0; i<x.size(); i++)
-		temp *= cos(xcoeff[i]*h[i]*x[i] + tcoeff[i]*t);
-	return temp;
+		// Set spatially-independent coefficients
+		dt = 0.01;
+		rho = 1.0;   // What units?
+		At[3] = 1.0; // fourth dimension is time -- cannot be zero, unless you implement an iterative solver for steady-state
+		Ck[0] = 1.0;    Ck[1] = 1.0;    Ck[2] = 1.0;
+		Cc[0] = 1.0;    Cc[1] = 1.0;    Cc[2] = 1.0;
+
+		// Set coeffients for each dimension. If a dimension does not exist, leave its value zero.
+		if (dim>0) { // x-axis
+			N[0]=1024;  h[0]=1.0/N[0];    Ax[0] = 1.0;    At[0] = 1.0;
+		}
+		if (dim>1) { // y-axis
+			N[0]=256;   h[0]=1.0/N[0];
+			N[1]=256;   h[1]=1.0/N[1];    Ax[1] = 1.0;    At[1] = 1.0;
+		}
+		if (dim>2) { // z-axis
+			N[0]=64;    h[0]=1.0/N[0];
+			N[1]=64;    h[1]=1.0/N[1];
+			N[2]=64;    h[2]=1.0/N[2];    Ax[2] = 1.0;    At[2] = 1.0;
+		}
 }
 
-void SourceQ (const vector<int> xidx, vector<double> h, double t,
-              vector<double> Ax, // spatial amplitudes
-              vector<double> At, // temporal amplitudes
-              vector<double> k,  // thermal diffusivities
-              vector<double> Cp, // heat capacities
-              double rho)
+void generate(int dim, const char* filename)
+{
+	if (dim==1) {
+		coefficients<1> vars;
+		GRID1D initGrid(0,0,vars.N[0]);
+		dx(initGrid,0)=vars.h[0];
+
+		for (int i=0; i<nodes(initGrid); i++) {
+			vector<int> x = MMSP::position(initGrid,n);
+			initGrid(n) = MS_T(x, 0, vars.h, vars.Ax, vars.At);
+		}
+
+		output(initGrid,filename);
+	}
+
+	if (dim==2) {
+		coefficients<2> vars;
+		GRID2D initGrid(0,0,vars.N[0],0,vars.N[1]);
+		dx(initGrid)=vars.h[0];
+		dy(initGrid)=vars.h[1];
+
+		for (int i=0; i<nodes(initGrid); i++) {
+			vector<int> x = MMSP::position(initGrid,n);
+			initGrid(n) = MS_T(x, 0, vars.h, vars.Ax, vars.At);
+		}
+
+		output(initGrid,filename);
+	}
+
+	if (dim==3) {
+		coefficients<3> vars;
+		GRID3D initGrid(0,0,vars.N[0],0,vars.N[1],0,vars.N[2]);
+		dx(initGrid)=vars.h[0];
+		dy(initGrid)=vars.h[1];
+		dz(initGrid)=vars.h[2];
+
+		for (int i=0; i<nodes(initGrid); i++) {
+			vector<int> x = MMSP::position(initGrid,n);
+			initGrid(n) = MS_T(x, 0, vars.h, vars.Ax, vars.At);
+		}
+
+		output(initGrid,filename);
+	}
+}
+
+template <int dim, typename T> void update(grid<dim,T>& oldGrid, int steps)
+{
+	int rank=0;
+    #ifdef MPI_VERSION
+    rank = MPI::COMM_WORLD.Get_rank();
+    #endif
+
+	coefficients<dim> vars;
+
+	ghostswap(oldGrid);
+
+	grid<dim,T> newGrid(oldGrid);
+
+	for (int step=0; step<steps; step++) {
+		if (rank==0)
+			print_progress(step, steps);
+
+		grid<dim,vector<T> > gradGrid(oldGrid,dim);
+		for (int n=0; n<nodes(oldGrid); n++) {
+			
+		}
+
+		for (int n=0; n<nodes(oldGrid); n++) {
+			newGrid(n) = 
+			phi-dt*M*(-r*phi+u*pow(phi,3)-K*laplacian(oldGrid,i));
+		}
+		swap(oldGrid,newGrid);
+		ghostswap(oldGrid);
+	}
+}
+
+double MS_T (const vector<int>& xidx, double t, const vector<double>& h, const vector<double>& Ax, const vector<double>& At)
+{
+	// Analytical temperature
+	vector<double> x(3,0.0); // most general case is 3D, zero by default to handle 1D and 2D naturally
+	for (int d=0; d<xidx.size(); d++)
+		x[d] = h[d]*xidx[d];
+	assert(Ax.size() == 3);
+	assert(At.size() == 4);
+	return (
+		cos(Ax[0] * x[0] + At[0] * t) * cos(Ax[1] * x[1] + At[1] * t) * cos(Ax[2] * x[2] + At[2] * t) * cos(At[3] * t)
+	);
+}
+
+double MS_k (const vector<double>& Ck, double temp)
+{
+	return Ck[0] + Ck[1]*temp + Ck[2]*temp*temp;
+}
+
+double MS_Cp (const vector<double>& Cc, double temp)
+{
+	return Cc[0] + Cc[1]*temp + Cc[2]*temp*temp;
+}
+
+vector<double> MS_GradT (const vector<int>& xidx, double t, const vector<double>& h, const vector<double>& Ax, const vector<double>& At)
+{
+	// Analytical temperature gradient
+	vector<double> x(3,0.0); // most general case is 3D, zero by default to handle 1D and 2D naturally
+	for (int d=0; d<xidx.size(); d++)
+		x[d] = h[d]*xidx[d];
+	assert(Ax.size() == 3);
+	assert(At.size() == 4);
+	gradT_an[0] = -Ax[0] * cos(At[3] * t) * cos(Ax[1] * x[1] + At[1] * t) * cos(Ax[2] * x[2] + At[2] * t) * sin(Ax[0] * x[0] + At[0] * t);
+	gradT_an[1] = -Ax[1] * cos(At[3] * t) * cos(Ax[0] * x[0] + At[0] * t) * cos(Ax[2] * x[2] + At[2] * t) * sin(Ax[1] * x[1] + At[1] * t);
+	gradT_an[2] = -Ax[2] * cos(At[3] * t) * cos(Ax[0] * x[0] + At[0] * t) * cos(Ax[1] * x[1] + At[1] * t) * sin(Ax[2] * x[2] + At[2] * t);
+}
+
+double MS_Q (const vector<int>& xidx, double t, double rho, const vector<double>& h,
+             const vector<double>& Ax, const vector<double>& At,
+             const vector<double>& Ck, const vector<double>& Cc)
 {
 	vector<double> x(3,0.0); // most general case is 3D, zero by default to handle 1D and 2D naturally
 	for (int d=0; d<xidx.size(); d++)
@@ -42,14 +167,11 @@ void SourceQ (const vector<int> xidx, vector<double> h, double t,
 	assert(Cp.size() == 3);
 	assert(At.size() == 4);
 
-	double Q_T;       // thermal source term
-	double T_an;      // analytical temperature
-	double *gradT_an; // analytical temperature gradient
-
 	// Define the thermal source term. Transformed for MMSP accessors and restructured for clarity and linewidth
-	// after https://github.com/manufactured-solutions/analytical/blob/master/heat_equation/C_code/Temp_3d_unsteady_k_cp_var.C
+	// after https://github.com/manufactured-solutions/analytical/tree/master/heat_equation/C_code/
 	// For best results, generate this yourself using a symbolic math package, e.g. SymPy, Mathematica, or Maple.
 	// All this floating-point math in a single expression makes me nervous.
+	double Q_T = 0.0;
 	Q_T = - rho * Cp[0] * At[0] * sin(Ax[0] * x[0] + At[0] * t)
 	                            * cos(Ax[1] * x[1] + At[1] * t)
 	                            * cos(Ax[2] * x[2] + At[2] * t)
@@ -66,22 +188,20 @@ void SourceQ (const vector<int> xidx, vector<double> h, double t,
 	                            * cos(Ax[1] * x[1] + At[1] * t)
 	                            * cos(Ax[2] * x[2] + At[2] * t)
 	                            * sin(At[3] * t)
-	      - k[1] * Ax[0] * Ax[0] * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
-	                             * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
-	                             * pow(cos(At[3] * t), 2.0)
-	      - k[1] * Ax[1] * Ax[1] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
-	                             * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
-	                             * pow(cos(At[3] * t), 2.0)
-	      - k[1] * Ax[2] * Ax[2] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
-	                             * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
-	                             * pow(cos(At[3] * t), 2.0)
-	      + 3.0 * k[2] * ( Ax[0] * Ax[0]
-	                     + Ax[1] * Ax[1]
-	                     + Ax[2] * Ax[2]
-	                     ) * pow(cos(Ax[0] * x[0] + At[0] * t), 3.0)
-	                       * pow(cos(Ax[1] * x[1] + At[1] * t), 3.0)
-	                       * pow(cos(Ax[2] * x[2] + At[2] * t), 3.0)
-	                       * pow(cos(At[3] * t), 3.0)
+	      - k[1] * Ax[0]*Ax[0] * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
+	                           * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
+	                           * pow(cos(At[3] * t), 2.0)
+	      - k[1] * Ax[1]*Ax[1] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
+	                           * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
+	                           * pow(cos(At[3] * t), 2.0)
+	      - k[1] * Ax[2]*Ax[2] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
+	                           * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
+	                           * pow(cos(At[3] * t), 2.0)
+	      + 3.0 * k[2] * ( Ax[0]*Ax[0] + Ax[1]*Ax[1] + Ax[2]*Ax[2] )
+	                   * pow(cos(Ax[0] * x[0] + At[0] * t), 3.0)
+	                   * pow(cos(Ax[1] * x[1] + At[1] * t), 3.0)
+	                   * pow(cos(Ax[2] * x[2] + At[2] * t), 3.0)
+	                   * pow(cos(At[3] * t), 3.0)
 	      + (
 	        - rho * Cp[1] * At[0] * sin(Ax[0] * x[0] + At[0] * t)
 	                              * cos(Ax[1] * x[1] + At[1] * t)
@@ -99,14 +219,14 @@ void SourceQ (const vector<int> xidx, vector<double> h, double t,
 	                              * cos(Ax[1] * x[1] + At[1] * t)
 	                              * cos(Ax[2] * x[2] + At[2] * t)
 	                              * sin(At[3] * t)
-	        + k[0] * Ax[0] * Ax[0] + k[0] * Ax[1] * Ax[1] + k[0] * Ax[2] * Ax[2]
-	        - 2.0 * k[2] * Ax[0] * Ax[0] * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
+	        + k[0] * Ax[0]*Ax[0] + k[0] * Ax[1]*Ax[1] + k[0] * Ax[2]*Ax[2]
+	        - 2.0 * k[2] * Ax[0]*Ax[0] * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
 	                                     * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
 	                                     * pow(cos(At[3] * t), 2.0)
-	        - 2.0 * k[2] * Ax[1] * Ax[1] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
+	        - 2.0 * k[2] * Ax[1]*Ax[1] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
 	                                     * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
 	                                     * pow(cos(At[3] * t), 2.0)
-	        - 2.0 * k[2] * Ax[2] * Ax[2] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
+	        - 2.0 * k[2] * Ax[2]*Ax[2] * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
 	                                     * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
 	                                     * pow(cos(At[3] * t), 2.0)
 	        ) * cos(Ax[0] * x[0] + At[0] * t)
@@ -130,81 +250,15 @@ void SourceQ (const vector<int> xidx, vector<double> h, double t,
 	                              * cos(Ax[1] * x[1] + At[1] * t)
 	                              * cos(Ax[2] * x[2] + At[2] * t)
 	                              * sin(At[3] * t)
-	        + 2.0 * k[1] * Ax[0] * Ax[0]
-	        + 2.0 * k[1] * Ax[1] * Ax[1]
-	        + 2.0 * k[1] * Ax[2] * Ax[2]
+	        + 2.0 * k[1] * Ax[0]*Ax[0]
+	        + 2.0 * k[1] * Ax[1]*Ax[1]
+	        + 2.0 * k[1] * Ax[2]*Ax[2]
 	        ) * pow(cos(Ax[0] * x[0] + At[0] * t), 2.0)
 	          * pow(cos(Ax[1] * x[1] + At[1] * t), 2.0)
 	          * pow(cos(Ax[2] * x[2] + At[2] * t), 2.0)
 	          * pow(cos(At[3] * t), 2.0);
 
-	T_an = cos(Ax[0] * x[0] + At[0] * t) * cos(Ax[1] * x[1] + At[1] * t) * cos(Ax[2] * x[2] + At[2] * t) * cos(At[3] * t);
-	gradT_an[0] = -Ax[0] * cos(At[3] * t) * cos(Ax[1] * x[1] + At[1] * t) * cos(Ax[2] * x[2] + At[2] * t) * sin(Ax[0] * x[0] + At[0] * t);
-	gradT_an[1] = -Ax[1] * cos(At[3] * t) * cos(Ax[0] * x[0] + At[0] * t) * cos(Ax[2] * x[2] + At[2] * t) * sin(Ax[1] * x[1] + At[1] * t);
-	gradT_an[2] = -Ax[2] * cos(At[3] * t) * cos(Ax[0] * x[0] + At[0] * t) * cos(Ax[1] * x[1] + At[1] * t) * sin(Ax[2] * x[2] + At[2] * t);
-}
-
-void generate(int dim, const char* filename)
-{
-	if (dim==1) {
-		int L=1024;
-		GRID1D initGrid(0,0,L);
-
-		for (int i=0; i<nodes(initGrid); i++)
-			initGrid(i) = 1.0-2.0*double(rand())/double(RAND_MAX);
-
-		output(initGrid,filename);
-	}
-
-	if (dim==2) {
-		int L=256;
-		GRID2D initGrid(0,0,2*L,0,L);
-
-		for (int i=0; i<nodes(initGrid); i++)
-			initGrid(i) = 1.0-2.0*double(rand())/double(RAND_MAX);
-
-		output(initGrid,filename);
-	}
-
-	if (dim==3) {
-		int L=64;
-		GRID3D initGrid(0,0,2*L,0,L,0,L/4);
-
-		for (int i=0; i<nodes(initGrid); i++)
-			initGrid(i) = 1.0-2.0*double(rand())/double(RAND_MAX);
-
-		output(initGrid,filename);
-	}
-}
-
-template <int dim, typename T> void update(grid<dim,T>& oldGrid, int steps)
-{
-	int rank=0;
-    #ifdef MPI_VERSION
-    rank = MPI::COMM_WORLD.Get_rank();
-    #endif
-
-	ghostswap(oldGrid);
-
-	grid<dim,T> newGrid(oldGrid);
-
-	double r = 1.0;
-	double u = 1.0;
-	double K = 1.0;
-	double M = 1.0;
-	double dt = 0.01;
-
-	for (int step=0; step<steps; step++) {
-		if (rank==0)
-			print_progress(step, steps);
-
-		for (int i=0; i<nodes(oldGrid); i++) {
-			T phi = oldGrid(i);
-			newGrid(i) = phi-dt*M*(-r*phi+u*pow(phi,3)-K*laplacian(oldGrid,i));
-		}
-		swap(oldGrid,newGrid);
-		ghostswap(oldGrid);
-	}
+	return Q_T;
 }
 
 } // namespace MMSP
--- examples/differential_equations/parabolic/heat/heat.hpp
+++ examples/differential_equations/parabolic/heat/heat.hpp
@@ -5,7 +5,49 @@
 std::string PROGRAM = "heat";
 std::string MESSAGE = "method of manufactured solutions using MMSP";
 
-typedef MMSP::grid<1,double> GRID1D;
-typedef MMSP::grid<2,double> GRID2D;
-typedef MMSP::grid<3,double> GRID3D;
+typedef MMSP::mmsgrid<1,double> GRID1D;
+typedef MMSP::mmsgrid<2,double> GRID2D;
+typedef MMSP::mmsgrid<3,double> GRID3D;
+
+// Create a templated Manufactured Solution-specific derived grid class
+template<int dim> class MMSP::coefficients {
+	public:
+		coefficients();    // constructor
+		vector<int> N;     // number of grid points in each direction
+		vector<double> h;  // grid spacing
+		vector<double> Ax; // amplitudes in space
+		vector<double> At; // amplitudes in time
+		vector<double> Ck; // polynomial coefficients for thermal conductivity
+		vector<double> Cc; // polynomial coefficients for heat capacity
+		double rho;        // material density (constant)
+};
+
+// temperature-dependent thermal conductivity
+double MS_k (const vector<double>& Ck, double temp);
+
+// temperature-dependent heat capacity
+double MS_Cp (const vector<double>& Cc, double temp);
+
+// analytical expression for manufactured temperature profile
+double MS_T (const vector<int>& xidx, double t,
+             const vector<double>& h,  // grid spacing
+             const vector<double>& Ax, // spatial amplitudes
+             const vector<double>& At // temporal amplitudes
+);
+
+// analytical expression for manufactured thermal source
+double MS_Q (const vector<int>& xidx, double t, double rho,
+             const vector<double>& h,  // grid spacing
+             const vector<double>& Ax, // spatial amplitudes
+             const vector<double>& At, // temporal amplitudes
+             const vector<double>& Ck, // thermal diffusivity polynomial coefficients
+             const vector<double>& Cc  // heat capacity polynomial coefficients
+);
+
+// analytical expression for gradient of manufactured temperature profile
+vector<double> MS_GradT (const vector<int>& xidx, double t,
+                         const vector<double>& h,  // grid spacing
+                         const vector<double>& Ax, // spatial amplitudes
+                         const vector<double>& At // temporal amplitudes
+);
 
